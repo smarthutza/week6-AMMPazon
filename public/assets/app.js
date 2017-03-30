481e4c1d8a65931d81df00d673b91c1d08259a04
PiotrBerebecki <petebere@gmail.com>
2017-03-30 18:15:53 +0100

    Show error on front end

--- public/assets/app.js
+++ public/assets/app.js
@@ -13,16 +13,16 @@ document.getElementById('js-select-analytics').onchange = function(e) {
 function renderData(err, results) {
   const resultsDOM = document.querySelector('.results__list');
   resultsDOM.innerHTML = '';
-  
-  if (err) { 
-    resultsDOM.innerHTML = '<li></li>';
+
+  if (err) {
+    resultsDOM.innerHTML = '<li>Sorry, database error</li>';
   }
-  
+
   resultsDOM.innerHTML = results.map(item => {
     return `
       <li>
         ${Object.keys(item).map(key => {
-          return `<span>${item[key]}</span>`;
+          return `<span>${key}: ${item[key]} </span>`;
         }).join('')}
       </li>
     `;
@@ -36,17 +36,17 @@ function renderData(err, results) {
 // **********************************************
 function fetch(method, url, cb) {
   var xhr = new XMLHttpRequest();
-  
+
   xhr.onreadystatechange = () => {
     if (xhr.readyState === 4 && xhr.status === 200) {
       cb(null, JSON.parse(xhr.responseText));
     }
   };
-  
+
   xhr.onerror = () => {
     cb(new Error('Database error'));
   };
-                 
+
   xhr.open(method, url, true);
   xhr.send();
 }
