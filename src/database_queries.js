50d40b10cb42317a4da8ec7ace747ecbc165090f
Akin909 <akin.sowemimo@gmail.com>
2017-03-30 11:52:34 +0100

    Add working get customers by spend query and function for queries
    relates #17 #21 #13

--- src/database_queries.js
+++ src/database_queries.js
@@ -3,9 +3,22 @@ const dbConnection = require('./../database/db_connection');
 
 const query = {};
 
+const getQuery = (url) => {
+  const queries =  {
+    bestSellers: ``,
+   customeryBySpend :`SELECT customers.firstname || ' ' || customers.surname AS name, SUM(products.price) AS total_spend FROM customers
+                      INNER JOIN orders ON customers.id = orders.customer_id
+                      INNER JOIN basket ON basket.order_id = orders.id
+                      INNER JOIN products  ON products.id = basket.product_id
+                      GROUP BY customers.firstname, customers.surname;`,
+    salesThisYear:``,
+    salesToDate:``
+  }
+  return queries[url];
+};
 
-query.getCustomersBySpend = (handlerCallback) => {
-  dbConnection.query('SELECT * FROM customers', (err, res) => {
+query.getCustomersBySpend = (url,handlerCallback) => {
+  dbConnection.query(getQuery(url), (err, res) => {
     if (err) {
       handlerCallback(err);
     } else {
