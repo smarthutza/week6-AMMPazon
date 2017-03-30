bbe5ec29a1ce52adc3b154d1c71b24cfd2242903
PiotrBerebecki <petebere@gmail.com>
2017-03-30 14:33:50 +0100

    Modify select form on the front and and setup route handling
    relates #25

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,5 +1,32 @@
 console.log('This works');
 
-document.getElementById('selection').onchange = function(){
-  console.log(document.getElementById('FORM_ID').action = '/'+this.value);
+document.getElementById('js-select-analytics').onchange = function(e) {
+  console.log(e.target.value);
+  fetch('GET', `get-data/${e.target.value}`, renderData);
 };
+
+
+
+// **********************************************
+// RENDER
+// **********************************************
+function renderData(err, res) {
+  console.log(res);
+}
+
+
+// **********************************************
+// FETCH
+// **********************************************
+function fetch(method, url, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      cb(null, xhr.responseText);
+    } else {
+      cb("error" + xhr.responseType);
+    }
+  };
+  xhr.open(method, url, true);
+  xhr.send();
+}
